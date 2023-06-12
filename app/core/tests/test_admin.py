776beaa8c2574932232bbf762a846f1a43a59702
Arshid <arshidbaba93@gmail.com>
2023-06-12 20:15:31 +0530

    Completed Admin Setup

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -29,6 +29,9 @@ class AdminSiteTests(TestCase):
         url = reverse("admin:core_user_changelist")
         res = self.client.get(url)
 
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
     def test_edit_user_page(self):
         """Test the edit user page works."""
         url = reverse("admin:core_user_change", args=[self.user.id])
